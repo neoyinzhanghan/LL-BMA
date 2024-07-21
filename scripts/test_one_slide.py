528012c04a210b32a1405d98ec474d2d5c1ae40b
Neo Yin <yinzhang@berkeley.edu>
2024-07-20 21:11:45 -0700

    test script on a single slide

--- scripts/test_one_slide.py
+++ scripts/test_one_slide.py
@@ -1,5 +1,5 @@
 import os
-from LLBMA.front_end.api import heme_analyze
+from LLBMA.front_end.api import analyse_bma
 
 slide_path = "/media/hdd1/neo/BMA_AML_lite/H17-4780;S10;MSKK - 2023-09-06 21.22.16.ndpi"
 dump_dir = "/media/hdd3/neo/LLBMA_dump_test"
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         os.makedirs(dump_dir)
 
     # Run the heme_analyze function 
-    heme_analyze(
+    analyse_bma(
         slide_path=slide_path,
         dump_dir=dump_dir,
         hoarding=True,
