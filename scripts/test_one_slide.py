7e6fae252f1947771b5f3a50062946048efbb394
Neo Yin <yinzhang@berkeley.edu>
2024-07-21 23:16:32 -0700

    differential config update and mpp assertion

--- scripts/test_one_slide.py
+++ scripts/test_one_slide.py
@@ -6,11 +6,15 @@ dump_dir = "/media/hdd3/neo/LLBMA_dump_test"
 
 if __name__ == "__main__":
 
-    # if the dump directory does not exist, create it 
+    # if the dump directory does not exist, create it
     if not os.path.exists(dump_dir):
         os.makedirs(dump_dir)
+    else:
+        # remove the dump directory if it already exists and recreate it
+        os.system(f"rm -rf {dump_dir}")
+        os.makedirs(dump_dir)
 
-    # Run the heme_analyze function 
+    # Run the heme_analyze function
     analyse_bma(
         slide_path=slide_path,
         dump_dir=dump_dir,
